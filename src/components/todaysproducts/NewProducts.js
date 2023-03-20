35c21a0c547446895d5eb2f6a20aa7d52d52c01e
Bhaskar Sawant <bhaskar.csawant417@gmail.com>
2023-03-20 21:25:33 +0530

    displayed fetched products to homepage

--- src/components/todaysproducts/NewProducts.js
+++ src/components/todaysproducts/NewProducts.js
@@ -2,17 +2,14 @@ import React from 'react'
 import ProductCard from '../productcard.js/ProductCard'
 import './newproducts.css'
 
-function NewProducts() {
+function NewProducts({ products }) {
     return (
         <>
             <div className="container_prouducts_2">
 
                 <h3 align='center' className="header_1">Products in today</h3>
                 <div className='products_container_2'>
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
+                    {products ? products.map(product => <ProductCard data={product} key={product._id} />) : null}
                 </div>
             </div>
         </>
