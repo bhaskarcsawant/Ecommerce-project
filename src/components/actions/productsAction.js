51ddb53b946b244427f94a3b8acf5b4d2e745eef
Bhaskar Sawant <bhaskar.csawant417@gmail.com>
2023-03-24 20:26:58 +0530

    added search functionality  in frontend

--- src/components/actions/productsAction.js
+++ src/components/actions/productsAction.js
@@ -2,11 +2,12 @@ import axios from "axios";
 import { ALL_PRODUCTS_REQUEST, ALL_PRODUCTS_SUCCESS, ALL_PRODUCTS_FAIL, PRODUCTS_DETAILS_REQUEST, PRODUCTS_DETAILS_SUCCESS, PRODUCTS_DETAILS_FAIL, CLEAR_ERRORS } from "../constants/productConstants";
 
 
-export const getProducts = async (dispatch) => {
+export const getProducts = (keyword = "") => async (dispatch) => {
     try {
         dispatch({ type: ALL_PRODUCTS_REQUEST })
+        let link = `/api/v1/products?keyword=${keyword}`
 
-        const { data } = await axios.get('/api/v1/products/')
+        const { data } = await axios.get(link)
 
         dispatch({
             type: ALL_PRODUCTS_SUCCESS,
