0c43ef532cfaed705de4d7818113e002849a2fe2
Bhaskar Sawant <bhaskar.csawant417@gmail.com>
2023-04-07 18:31:20 +0530

    added saveShippingInfo constant, added saveShippingInfo reducer, added saveShippingInfo  action.

--- src/components/reducers/cartReducer.js
+++ src/components/reducers/cartReducer.js
@@ -1,4 +1,8 @@
-import { ADD_TO_CART, REMOVE_FROM_CART } from "../constants/cartConstants";
+import {
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+  SAVE_SHIPPING_INFO,
+} from "../constants/cartConstants";
 
 
 export const cartReducer = (state = { cartItems: [] }, action) => { 
@@ -21,6 +25,12 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
                 return {
                     ...state,
                     cartItems: state.cartItems.filter((i) => i.product !== action.payload)
+            }
+        
+      case SAVE_SHIPPING_INFO:
+                return {
+                    ...state,
+                    shippingInfo:action.payload,
                 }
 
       default:
